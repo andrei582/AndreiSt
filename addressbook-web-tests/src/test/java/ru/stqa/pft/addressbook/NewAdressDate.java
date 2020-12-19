6bce475c10957fd646f40e922779e85080e4ecbf
andrei582 <72387558+andrei582@users.noreply.github.com>
2020-12-19 23:06:03 +0300

    создан объект NewAddressDate

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NewAdressDate.java
@@ -0,0 +1,109 @@
+package ru.stqa.pft.addressbook;
+
+public class NewAdressDate {
+  private final String name;
+  private final String middle;
+  private final String lastname;
+  private final String nickename;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String phoneHome;
+  private final String phoneMobile;
+  private final String phoneWork;
+  private final String fax;
+  private final String eAddress1;
+  private final String eAddress2;
+  private final String dayBirsday;
+  private final String mouseBirsday;
+  private final String yearBirsday;
+  private final String coments;
+
+  public NewAdressDate(String name, String middle, String lastname, String nickename, String title, String company, String address, String phoneHome, String phoneMobile, String phoneWork, String fax, String eAddress1, String eAddress2, String dayBirsday, String mouseBirsday, String yearBirsday, String coments) {
+    this.name = name;
+    this.middle = middle;
+    this.lastname = lastname;
+    this.nickename = nickename;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.phoneHome = phoneHome;
+    this.phoneMobile = phoneMobile;
+    this.phoneWork = phoneWork;
+    this.fax = fax;
+    this.eAddress1 = eAddress1;
+    this.eAddress2 = eAddress2;
+    this.dayBirsday = dayBirsday;
+    this.mouseBirsday = mouseBirsday;
+    this.yearBirsday = yearBirsday;
+    this.coments = coments;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getMiddle() {
+    return middle;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickename() {
+    return nickename;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhoneHome() {
+    return phoneHome;
+  }
+
+  public String getPhoneMobile() {
+    return phoneMobile;
+  }
+
+  public String getPhoneWork() {
+    return phoneWork;
+  }
+
+  public String getFax() {
+    return fax;
+  }
+
+  public String geteAddress1() {
+    return eAddress1;
+  }
+
+  public String geteAddress2() {
+    return eAddress2;
+  }
+
+  public String getDayBirsday() {
+    return dayBirsday;
+  }
+
+  public String getMouseBirsday() {
+    return mouseBirsday;
+  }
+
+  public String getYearBirsday() {
+    return yearBirsday;
+  }
+
+  public String getComents() {
+    return coments;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NumberCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/NumberCreationTest.java
@@ -1,105 +1,122 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class NumberCreationTest {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+  private WebDriver wb;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wb = new FirefoxDriver();
+    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    input("admin", "secret");
+
+  }
+
+  private void input(String username, String password) {
+    wb.get("http://localhost/addressbook/");
+    wb.findElement(By.name("user")).clear();
+    wb.findElement(By.name("user")).sendKeys(username);
+    wb.findElement(By.name("pass")).click();
+    wb.findElement(By.name("pass")).clear();
+    wb.findElement(By.name("pass")).sendKeys(password);
+    submitNewAddress("//input[@value='Login']");
   }
 
   @Test
   public void testNumberCreation() throws Exception {
-    driver.get("http://localhost/addressbook/");
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-    driver.findElement(By.linkText("add new")).click();
-    driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys("ttname");
-    driver.findElement(By.name("middlename")).click();
-    driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys("ttmiddle");
-    driver.findElement(By.name("lastname")).click();
-    driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys("ttlast");
-    driver.findElement(By.name("nickname")).click();
-    driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys("ttnicke");
-    driver.findElement(By.name("title")).click();
-    driver.findElement(By.name("title")).clear();
-    driver.findElement(By.name("title")).sendKeys("tttitle");
-    driver.findElement(By.name("company")).click();
-    driver.findElement(By.name("company")).clear();
-    driver.findElement(By.name("company")).sendKeys("ttcompany");
-    driver.findElement(By.name("address")).click();
-    driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys("ttaddress at the test");
-    driver.findElement(By.name("theform")).click();
-    driver.findElement(By.name("home")).click();
-    driver.findElement(By.name("home")).clear();
-    driver.findElement(By.name("home")).sendKeys("444555");
-    driver.findElement(By.name("mobile")).click();
-    driver.findElement(By.name("mobile")).clear();
-    driver.findElement(By.name("mobile")).sendKeys("44445555");
-    driver.findElement(By.name("work")).click();
-    driver.findElement(By.name("work")).clear();
-    driver.findElement(By.name("work")).sendKeys("445544");
-    driver.findElement(By.name("fax")).click();
-    driver.findElement(By.name("fax")).clear();
-    driver.findElement(By.name("fax")).sendKeys("44455554");
-    driver.findElement(By.name("email")).click();
-    driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys("4545@444.44");
-    driver.findElement(By.name("email2")).click();
-    driver.findElement(By.name("email2")).clear();
-    driver.findElement(By.name("email2")).sendKeys("4445@445.45");
-    driver.findElement(By.name("bday")).click();
-    new Select(driver.findElement(By.name("bday"))).selectByVisibleText("4");
-    driver.findElement(By.name("bday")).click();
-    driver.findElement(By.name("bmonth")).click();
-    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText("April");
-    driver.findElement(By.name("bmonth")).click();
-    driver.findElement(By.name("byear")).click();
-    driver.findElement(By.name("byear")).clear();
-    driver.findElement(By.name("byear")).sendKeys("4444");
-    driver.findElement(By.name("notes")).click();
-    driver.findElement(By.name("notes")).clear();
-    driver.findElement(By.name("notes")).sendKeys("444 nnn hhh");
-    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    driver.findElement(By.linkText("home")).click();
-    driver.findElement(By.linkText("Logout")).click();
+    gotoNewAddress();
+    fillNewAdressForm(new NewAdressDate("ttname", "ttmiddle", "ttlast", "ttnicke", "tttitle", "ttcompany", "ttaddress at the test", "444555", "44445555", "445544", "44455554", "4545@444.44", "4445@445.45", "4", "April", "4444", "444 nnn hhh"));
+    submitNewAddress("(//input[@name='submit'])[2]");
+    gotoHome();
+    gotoLogAut();
   }
 
-  @AfterClass(alwaysRun = true)
+  private void gotoLogAut() {
+    wb.findElement(By.linkText("Logout")).click();
+  }
+
+  private void gotoHome() {
+    wb.findElement(By.linkText("home")).click();
+  }
+
+  private void submitNewAddress(String s) {
+    wb.findElement(By.xpath(s)).click();
+  }
+
+  private void fillNewAdressForm(NewAdressDate newAdressDate) {
+    wb.findElement(By.name("firstname")).click();
+    wb.findElement(By.name("firstname")).clear();
+    wb.findElement(By.name("firstname")).sendKeys(newAdressDate.getName());
+    wb.findElement(By.name("middlename")).click();
+    wb.findElement(By.name("middlename")).clear();
+    wb.findElement(By.name("middlename")).sendKeys(newAdressDate.getMiddle());
+    wb.findElement(By.name("lastname")).click();
+    wb.findElement(By.name("lastname")).clear();
+    wb.findElement(By.name("lastname")).sendKeys(newAdressDate.getLastname());
+    wb.findElement(By.name("nickname")).click();
+    wb.findElement(By.name("nickname")).clear();
+    wb.findElement(By.name("nickname")).sendKeys(newAdressDate.getNickename());
+    wb.findElement(By.name("title")).click();
+    wb.findElement(By.name("title")).clear();
+    wb.findElement(By.name("title")).sendKeys(newAdressDate.getTitle());
+    wb.findElement(By.name("company")).click();
+    wb.findElement(By.name("company")).clear();
+    wb.findElement(By.name("company")).sendKeys(newAdressDate.getCompany());
+    wb.findElement(By.name("address")).click();
+    wb.findElement(By.name("address")).clear();
+    wb.findElement(By.name("address")).sendKeys(newAdressDate.getAddress());
+    wb.findElement(By.name("theform")).click();
+    wb.findElement(By.name("home")).click();
+    wb.findElement(By.name("home")).clear();
+    wb.findElement(By.name("home")).sendKeys(newAdressDate.getPhoneHome());
+    wb.findElement(By.name("mobile")).click();
+    wb.findElement(By.name("mobile")).clear();
+    wb.findElement(By.name("mobile")).sendKeys(newAdressDate.getPhoneMobile());
+    wb.findElement(By.name("work")).click();
+    wb.findElement(By.name("work")).clear();
+    wb.findElement(By.name("work")).sendKeys(newAdressDate.getPhoneWork());
+    wb.findElement(By.name("fax")).click();
+    wb.findElement(By.name("fax")).clear();
+    wb.findElement(By.name("fax")).sendKeys(newAdressDate.getFax());
+    wb.findElement(By.name("email")).click();
+    wb.findElement(By.name("email")).clear();
+    wb.findElement(By.name("email")).sendKeys(newAdressDate.geteAddress1());
+    wb.findElement(By.name("email2")).click();
+    wb.findElement(By.name("email2")).clear();
+    wb.findElement(By.name("email2")).sendKeys(newAdressDate.geteAddress2());
+    wb.findElement(By.name("bday")).click();
+    new Select(wb.findElement(By.name("bday"))).selectByVisibleText(newAdressDate.getDayBirsday());
+    wb.findElement(By.name("bday")).click();
+    wb.findElement(By.name("bmonth")).click();
+    new Select(wb.findElement(By.name("bmonth"))).selectByVisibleText(newAdressDate.getMouseBirsday());
+    wb.findElement(By.name("bmonth")).click();
+    wb.findElement(By.name("byear")).click();
+    wb.findElement(By.name("byear")).clear();
+    wb.findElement(By.name("byear")).sendKeys(newAdressDate.getYearBirsday());
+    wb.findElement(By.name("notes")).click();
+    wb.findElement(By.name("notes")).clear();
+    wb.findElement(By.name("notes")).sendKeys(newAdressDate.getComents());
+  }
+
+  private void gotoNewAddress() {
+    wb.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
+    wb.quit();
   }
 
   private boolean isElementPresent(By by) {
     try {
-      driver.findElement(by);
+      wb.findElement(by);
       return true;
     } catch (NoSuchElementException e) {
       return false;
@@ -108,25 +125,11 @@ public class NumberCreationTest {
 
   private boolean isAlertPresent() {
     try {
-      driver.switchTo().alert();
+      wb.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
     }
   }
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
 }
