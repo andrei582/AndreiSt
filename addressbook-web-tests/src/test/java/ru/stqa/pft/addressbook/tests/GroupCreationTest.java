b29a337728fc8d3ba548fd0de298baec5695866a
andrei582 <72387558+andrei582@users.noreply.github.com>
2021-01-02 22:13:42 +0300

    введен контроль количества элементов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
@@ -8,10 +9,13 @@ public class GroupCreationTest extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
+    int before = applicationMenager.getGroupHelper().getGroupCount();
     applicationMenager.getNavigationHelper().gotoGroupPage();
     applicationMenager.getGroupHelper().createGroup(new GroupDate("test21", null, null));
     applicationMenager.getNavigationHelper().gotoGroupPage();
     applicationMenager.getNavigationHelper().gotoLogAut();
+    int after = applicationMenager.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, ++before);
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDelitionTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
@@ -8,6 +9,7 @@ public class GroupDelitionTest extends TestBase {
   @Test
   public void testGroupDelition() throws Exception {
     applicationMenager.getNavigationHelper().gotoGroupPage();
+    int before = applicationMenager.getGroupHelper().getGroupCount();
     if (! applicationMenager.getGroupHelper().isThereAGroup() ){
       applicationMenager.getGroupHelper().createGroup(new GroupDate("test21", null, null));
       applicationMenager.getNavigationHelper().gotoGroupPage();
@@ -15,6 +17,10 @@ public class GroupDelitionTest extends TestBase {
     applicationMenager.getGroupHelper().selectGroup();
     applicationMenager.delitSelectedGroup();
     applicationMenager.getNavigationHelper().gotoGroupPage();
+    if (before != 0) {
+      int after = applicationMenager.getGroupHelper().getGroupCount();
+      Assert.assertEquals(after, before - 1);
+    }
     applicationMenager.getNavigationHelper().gotoLogAut();
   }
 
